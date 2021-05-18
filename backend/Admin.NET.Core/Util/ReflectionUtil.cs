4fd178d609f714e50e714467f98df23bff9fb8bc
TaoRan <staoran@gmail.com>
2021-05-18 21:50:16 +0800

    获取特性方法增加一个获取类型特性的重载方法

--- backend/Admin.NET.Core/Util/ReflectionUtil.cs
+++ backend/Admin.NET.Core/Util/ReflectionUtil.cs
@@ -22,5 +22,20 @@ namespace Admin.NET.Core
             // 如果没有数据返回null
             return customAttributes.Length > 0 ? (T)customAttributes[0] : null;
         }
+        
+        /// <summary>
+        /// 获取类型的特性
+        /// </summary>
+        /// <param name="field"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetDescriptionValue<T>(this Type field) where T : Attribute
+        {
+            // 获取字段的指定特性，不包含继承中的特性
+            object[] customAttributes = field.GetCustomAttributes(typeof(T), false);
+
+            // 如果没有数据返回null
+            return customAttributes.Length > 0 ? (T)customAttributes[0] : null;
+        }
     }
 }
