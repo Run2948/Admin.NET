5361cb9c40ceb23731385b88ebf3bc4386b660bc
zhujinrun <15997352948@163.com>
2021-07-08 00:13:12 +0800

    同步修复模板项目中的相关问题

--- template/Admin.VueTemplate/src/vue.config.js
+++ template/Admin.VueTemplate/src/vue.config.js
@@ -19,7 +19,7 @@ const assetsCDN = {
   css: [],
   // https://unpkg.com/browse/vue@2.6.10/
   js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+    '//cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js',
     '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
     '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
     '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
