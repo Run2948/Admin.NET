a684e4a0c5578f5b05f1535551115e597e724245
许俊杰 <xjj0906@vip.qq.com>
2021-07-16 09:15:06 +0800

    feat: 增加租户删除判断，防止删除默认租户导致系统不可用

--- backend/Furion.Extras.Admin.NET/SeedData/SysUserRoleSeedData.cs
+++ backend/Furion.Extras.Admin.NET/SeedData/SysUserRoleSeedData.cs
@@ -18,7 +18,8 @@ namespace Furion.Extras.Admin.NET.SeedData
             return new[]
             {
                 // 租户管理员默认管理员角色
-                new SysUserRole{SysUserId=142307070910554, SysRoleId=142307070910556 }
+                new SysUserRole {SysUserId = 142307070910552, SysRoleId = 142307070910554},
+                new SysUserRole {SysUserId = 142307070910554, SysRoleId = 142307070910556}
             };
         }
     }
