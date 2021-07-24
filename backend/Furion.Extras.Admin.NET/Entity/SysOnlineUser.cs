a3fb429aad42fde73862022e90138d51beaa5775
许俊杰 <xjj0906@vip.qq.com>
2021-07-24 12:36:17 +0800

    feat: 实现强制下线功能

--- backend/Furion.Extras.Admin.NET/Entity/SysOnlineUser.cs
+++ backend/Furion.Extras.Admin.NET/Entity/SysOnlineUser.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Furion.Extras.Admin.NET
@@ -11,7 +12,7 @@ namespace Furion.Extras.Admin.NET
     /// </summary>
     [Table("sys_online_user")]
     [Comment("在线用户表")]
-    public class OnlineUser : IEntity, IEntityTypeBuilder<OnlineUser>
+    public class OnlineUser //: IEntity, IEntityTypeBuilder<OnlineUser>
     {
         /// <summary>
         /// 连接Id
@@ -26,10 +27,51 @@ namespace Furion.Extras.Admin.NET
         public long UserId { get; set; }
 
         /// <summary>
+        /// 账号
+        /// </summary>
+        [Comment("账号")]
+        [Required, MaxLength(20)]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Comment("姓名")]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        /// <summary>
         /// 最后连接时间
         /// </summary>
         [Comment("最近时间")]
         public DateTime LastTime { get; set; }
+
+        /// <summary>
+        /// 最后登录IP
+        /// </summary>
+        [Comment("最后登录IP")]
+        [MaxLength(50)]
+        public string LastLoginIp { get; set; }
+
+        /// <summary>
+        /// 最后登录所用浏览器
+        /// </summary>
+        [Comment("最后登录所用浏览器")]
+        [MaxLength(20)]
+        public string LastLoginBrowser { get; set; }
+
+        /// <summary>
+        /// 最后登录所用系统
+        /// </summary>
+        [Comment("最后登录所用系统")]
+        [MaxLength(20)]
+        public string LastLoginOs { get; set; }
+
+        /// <summary>
+        /// 租户id
+        /// </summary>
+        [Comment("租户id")]
+        public long TenantId { get; set; }
 
         public void Configure(EntityTypeBuilder<OnlineUser> entityBuilder, DbContext dbContext, Type dbContextLocator)
         {
--- frontend/src/api/modular/system/onlineUserManage.js
+++ frontend/src/api/modular/system/onlineUserManage.js
@@ -3,6 +3,20 @@ import {
 } from '@/utils/request'
 
 /**
+ * 分页在线用户列表
+ *
+ * @author 写意
+ * @date 2021/7/21 23:34
+ */
+export function sysOnlineUserPage(parameter) {
+  return axios({
+    url: '/sysOnlineUser/page',
+    method: 'get',
+    params: parameter
+  })
+}
+
+/**
  * 在线用户列表
  *
  * @author yubaoshan
