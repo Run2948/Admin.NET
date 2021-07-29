fe57e667dc240cf13729015026935c90cf7e3400
chenjingchun <chenjingchun@foxmail.com>
2021-07-29 15:58:12 +0800

    修改时，排除创建人和创建时间

--- backend/Admin.NET.EntityFramework.Core/DbContexts/DefaultDbContext.cs
+++ backend/Admin.NET.EntityFramework.Core/DbContexts/DefaultDbContext.cs
@@ -96,9 +96,15 @@ namespace Admin.NET.EntityFramework.Core
                                 obj.CreatedUserName = userName;
                             }
                             break;
-                        // 排除租户Id
                         case EntityState.Modified:
-                            entity.Property(nameof(Entity.TenantId)).IsModified = false;
+                            // 排除租户Id
+                            entity.Property(nameof(DEntityTenant.TenantId)).IsModified = false;
+                            // 排除创建人
+                            entity.Property(nameof(DEntityTenant.CreatedUserId)).IsModified = false;
+                            entity.Property(nameof(DEntityTenant.CreatedUserName)).IsModified = false;
+                            // 排除创建日期
+                            entity.Property(nameof(DEntityTenant.UpdatedTime)).IsModified = false;
+
                             obj.UpdatedTime = DateTimeOffset.Now;
                             if (!string.IsNullOrEmpty(userId))
                             {
@@ -123,6 +129,12 @@ namespace Admin.NET.EntityFramework.Core
                     }
                     else if (entity.State == EntityState.Modified)
                     {
+                        // 排除创建人
+                        entity.Property(nameof(DEntityBase.CreatedUserId)).IsModified = false;
+                        entity.Property(nameof(DEntityBase.CreatedUserName)).IsModified = false;
+                        // 排除创建日期
+                        entity.Property(nameof(DEntityBase.UpdatedTime)).IsModified = false;
+
                         obj.UpdatedTime = DateTimeOffset.Now;
                         if (!string.IsNullOrEmpty(userId))
                         {
