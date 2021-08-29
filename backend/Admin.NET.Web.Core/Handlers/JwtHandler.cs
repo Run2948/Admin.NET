351b916a56d3f68a8490b54776ffef1e0f9aeca8
zhujinrun <15997352948@163.com>
2021-08-29 19:07:44 +0800

    😉更新脚手架到 1.0.33

--- backend/Admin.NET.Web.Core/Handlers/JwtHandler.cs
+++ backend/Admin.NET.Web.Core/Handlers/JwtHandler.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Furion.Extras.Admin.NET.Options;
 
 namespace Admin.NET.Web.Core
 {
