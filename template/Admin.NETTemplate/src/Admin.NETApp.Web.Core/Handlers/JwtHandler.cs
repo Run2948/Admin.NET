20f33d11f19900f81b9caaddf8df99137b0f8a70
zhujinrun <15997352948@163.com>
2021-09-24 21:05:21 +0800

    为所有用户添加用户选择器权限

--- template/Admin.NETTemplate/src/Admin.NETApp.Web.Core/Handlers/JwtHandler.cs
+++ template/Admin.NETTemplate/src/Admin.NETApp.Web.Core/Handlers/JwtHandler.cs
@@ -74,6 +74,8 @@ namespace Admin.NETApp.Web.Core
 
             // 获取用户权限集合（按钮或API接口）
             var allPermissionList = await App.GetService<ISysMenuService>().GetAllPermissionList();
+            allPermissionList.Add("sysUser:selector");//为所有用户添加用户选择器权限
+
             var permissionList = await App.GetService<ISysMenuService>().GetLoginPermissionList(userManager.UserId);
 
             // 检查授权
